f8c324c54521a2f48b3952749f94928893211831
github username <scott.gloyna01@gmail.com>
2021-02-25 20:09:34 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 function superbowlWin(array) {
     let winYear = array.find(game => game.result == "W")
-    if(winYear){
-        return winYear.year
-    }
-    return winYear
+   return winYear? winYear.year
+
 }
